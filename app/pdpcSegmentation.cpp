564bd492372a7c73b4a382b545d9170a85edeab9
T. Lejemble <thibault.lejemble@irit.fr>
2020-12-14 12:37:21 +0100

    tmp

--- app/pdpcSegmentation.cpp
+++ app/pdpcSegmentation.cpp
@@ -3,6 +3,7 @@
 #include <PDPC/PointCloud/Loader.h>
 #include <PDPC/PointCloud/PointCloud.h>
 #include <PDPC/MultiScaleFeatures/MultiScaleFeatures.h>
+#include <PDPC/Segmentation/SeededKNNGraphRegionGrowing.h>
 
 using namespace pdpc;
 
@@ -11,6 +12,9 @@ int main(int argc, char **argv)
     Option opt(argc, argv);
     const std::string in_input    = opt.get_string("input",    "i").set_brief("Input point cloud (.ply/.obj)").set_required();
     const std::string in_features = opt.get_string("features", "f").set_brief("Input features (.txt/.bin)").set_required();
+
+    const int    in_k     = opt.get_int(  "knn",  "k").set_default(10).set_brief("Region growing nearest neighbors count");
+    const Scalar in_theta = opt.get_float("theta"    ).set_default(5 ).set_brief("Region growing angular threshold (degrees)");
 
     const bool in_v = opt.get_bool("verbose", "v").set_default(false).set_brief("Add verbose messages");
 
@@ -34,6 +38,18 @@ int main(int argc, char **argv)
     }
 
 
+    // 1. Segmentations --------------------------------------------------------
+    info().iff(in_v) << "Performing " << scale_count << " planar region growing";
+
+    points.build_knn_graph(in_k);
+
+    for(int j=0; j<scale_count; ++j)
+    {
+        SeededKNNGraphRegionGrowing::compute(points, seg,
+        [](){},
+        [](){},
+        [](){});
+    }
 
     return 0;
 }
