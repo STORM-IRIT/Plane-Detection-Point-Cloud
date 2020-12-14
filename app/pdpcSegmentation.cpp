7b368ef2977a3a8f3eae4b0835419b52303c1c54
T. Lejemble <thibault.lejemble@irit.fr>
2020-12-14 14:57:53 +0100

    component extraction

--- app/pdpcSegmentation.cpp
+++ app/pdpcSegmentation.cpp
@@ -7,6 +7,9 @@
 #include <PDPC/ScaleSpace/ScaleSampling.h>
 #include <PDPC/Segmentation/SeededKNNGraphRegionGrowing.h>
 #include <PDPC/Segmentation/MSSegmentation.h>
+#include <PDPC/Segmentation/MSSegmentationGraph.h>
+#include <PDPC/Graph/HierarchicalGraph.h>
+#include <PDPC/Persistence/ComponentSet.h>
 
 using namespace pdpc;
 
@@ -20,6 +23,9 @@ int main(int argc, char **argv)
     const int    in_k     = opt.get_int(  "knn",  "k").set_default(10).set_brief("Region growing nearest neighbors count");
     const Scalar in_theta = opt.get_float("theta"    ).set_default(5.).set_brief("Region growing angular threshold (degrees)");
     const Scalar in_phi   = opt.get_float("phi"      ).set_default(1.).set_brief("Region growing curvature threshold");
+
+    const Scalar in_j_min = opt.get_float("jaccard_min", "jmin").set_default(0.5).set_brief("Jaccard index min threshold");
+    const int    in_p_min = opt.get_int(  "pers_min",    "pmin").set_default(2).set_brief("Persistence min threshold");
 
     const bool in_v = opt.get_bool("verbose", "v").set_default(false).set_brief("Add verbose messages");
 
@@ -53,7 +59,6 @@ int main(int argc, char **argv)
     // 1. Segmentations --------------------------------------------------------
     info().iff(in_v) << "Performing " << scale_count << " planar region growing";
     MSSegmentation ms_seg(scale_count, Segmentation(point_count));
-
     {
         const Scalar threshold_angle = std::cos(in_theta / 180. * M_PI);
         const Scalar threshold_curva = in_phi;
@@ -120,9 +125,108 @@ int main(int argc, char **argv)
 
     // 2. Graph ----------------------------------------------------------------
     info().iff(in_v) << "Building graph";
+    HierarchicalGraph g;
+    MSSegmentationGraph::create(ms_seg, g);
+
+    // 3. Component extraction -------------------------------------------------
+    info().iff(in_v) << "Extracting component";
+    ComponentSet comp_set(g.node_count(0));
     {
-        PDPC_TODO;
+        PDPC_ASSERT(g.node_properties(0).has("size"));
+        PDPC_ASSERT(g.edge_properties(0).has("weight"));
+        const int prop_size   = g.node_properties(0).index("size");
+        const int prop_weight = g.edge_properties(0).index("weight");
+
+        int level = 0;
+        std::vector<int> region_to_comp(g.node_count(level));
+        std::vector<int> region_to_comp2;
+
+        for(int i=0; i<comp_set.size(); ++i)
+        {
+            comp_set[i].initialize(level, i);
+            region_to_comp[i] = i;
+        }
+
+        //       vvv                                      vvvv
+        for(level=1; level<g.level_count(); ++level, std::swap(region_to_comp,region_to_comp2))
+        {
+            PDPC_DEBUG_ASSERT(!has_duplicate(region_to_comp));
+            PDPC_DEBUG_ASSERT(std::all_of(region_to_comp.begin(), region_to_comp.end(), [&](int idx_comp)->bool{return 0 <= idx_comp && idx_comp < comp_set.size() && comp_set[idx_comp].death_level() == level-1;}));
+
+            std::vector<int> indices(g.edge_count(level-1));
+            std::iota(indices.begin(), indices.end(), int(0));
+            std::sort(indices.begin(), indices.end(), [&](int i, int j)->bool
+            {
+                const Scalar coeff_i = MSSegmentationGraph::jaccard(g, level-1, i, prop_size, prop_weight);
+                const Scalar coeff_j = MSSegmentationGraph::jaccard(g, level-1, j, prop_size, prop_weight);
+
+                return coeff_i > coeff_j;
+            });
+
+            region_to_comp2.resize(g.node_count(level)); //           vvv
+            std::fill(region_to_comp2.begin(), region_to_comp2.end(), -1);
+
+            for(int idx_edge : indices)
+            {
+                const int idx_source = g.source(level-1, idx_edge);
+                const int idx_target = g.target(level-1, idx_edge);
+                const int idx_comp   = region_to_comp[idx_target];
+
+                PDPC_DEBUG_ASSERT(g.contains_node(level,   idx_source));
+                PDPC_DEBUG_ASSERT(g.contains_node(level-1, idx_target));
+                PDPC_DEBUG_ASSERT(0 <= idx_comp && idx_comp < comp_set.size());
+
+                // no component has the source node  &&  the candidate component has no node at level
+                if(region_to_comp2[idx_source] == -1 && comp_set[idx_comp].death_level() == level-1)
+                {
+                    Scalar coeff = MSSegmentationGraph::jaccard(g, level-1, idx_edge, prop_size, prop_weight);
+                    if(in_j_min <= coeff)
+                    {
+                        // expand current component
+                        comp_set[idx_comp].push_back(idx_source);
+                        region_to_comp2[idx_source] = idx_comp;
+                    }
+                    else
+                    {
+                        // stop current component = create a new one
+                        int idx_comp_new = comp_set.size();
+                        comp_set.push_back();
+                        comp_set.back().initialize(level, idx_source);
+                        region_to_comp2[idx_source] = idx_comp_new;
+                    }
+                }
+            }
+
+            // create new components if needed
+            for(int idx_node=0; idx_node<g.node_count(level); ++idx_node)
+            {
+                if(region_to_comp2[idx_node] == -1)
+                {
+                    int idx_comp = comp_set.size();
+                    comp_set.push_back();
+                    comp_set[idx_comp].initialize(level, idx_node);
+                    region_to_comp2[idx_node] = idx_comp;
+                }
+            }
+        }
+
+        // sort
+        PDPC_ASSERT(comp_set.properties().count() == 0);
+        std::sort(comp_set.data().begin(), comp_set.data().end(), [](const auto& x, const auto& y)
+        {
+            return x.persistence() > y.persistence();
+        });
+
+        // filter
+        auto it = std::find_if(comp_set.data().begin(), comp_set.data().end(), [in_p_min](const auto& x)
+        {
+            return x.persistence() < in_p_min;
+        });
+        comp_set.data().erase(it, comp_set.data().end());
+        comp_set.properties().resize(comp_set.data().size());
     }
+
+
 
     return 0;
 }
