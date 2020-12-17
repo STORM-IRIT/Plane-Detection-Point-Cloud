0b0d26bb8b5d22d6d581f6cc72618a48d532a45d
T. Lejemble <thibault.lejemble@irit.fr>
2020-12-17 16:20:58 +0100

    correct persistence thresholds

--- figures/generate_fig_09c.sh
+++ figures/generate_fig_09c.sh
@@ -4,4 +4,4 @@ set -ex
 
 pdpcComputeMultiScaleFeatures -i ../data/lans.ply -o data_09c -v  
 pdpcSegmentation -i ../data/lans.ply -s data_09c_scales.txt -f data_09c_features.bin -o data_09c -v 
-pdpcPostProcess -i ../data/lans.ply -s data_09c_seg.bin -r data_09c_reg.bin -c data_09c_comp.bin -o fig_09c -col -v -pers 1 2 3 4 5 6 7 8 9 10 15
+pdpcPostProcess -i ../data/lans.ply -s data_09c_seg.bin -r data_09c_reg.bin -c data_09c_comp.bin -o fig_09c -col -v -pers 5 10 15
