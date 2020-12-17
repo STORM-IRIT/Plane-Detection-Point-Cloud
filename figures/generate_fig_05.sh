c0a90ee8cbd58c60e576c5d3fe16f826f720fb35
T. Lejemble <thibault.lejemble@irit.fr>
2020-12-17 15:23:21 +0100

    remove input variable from scripts

--- figures/generate_fig_05.sh
+++ figures/generate_fig_05.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-input=../data/triangles.ply
-
-pdpcComputeMultiScaleFeatures -i $input -o data_05 -v
-pdpcSegmentation -i $input -s data_05_scales.txt -f data_05_features.bin -o data_05 -v 
-pdpcPostProcess -i $input -s data_05_seg.bin -r data_05_reg.bin -c data_05_comp.bin -o fig_05 -col -v -range 20 24 25 30 40 42
-
+pdpcComputeMultiScaleFeatures -i ../data/triangles.ply -o data_05 -v
+pdpcSegmentation -i ../data/triangles.ply -s data_05_scales.txt -f data_05_features.bin -o data_05 -v 
+pdpcPostProcess -i ../data/triangles.ply -s data_05_seg.bin -r data_05_reg.bin -c data_05_comp.bin -o fig_05 -col -v -range 20 24 25 30 40 42
--- figures/generate_fig_15b.sh
+++ figures/generate_fig_15b.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-input=../data/lans.ply
-
-pdpcComputeMultiScaleFeatures -i $input -o data_15b -v  
-pdpcSegmentation -i $input -s data_15b_scales.txt -f data_15b_features.bin -o data_15b -v 
-pdpcPostProcess -i $input -s data_15b_seg.bin -r data_15b_reg.bin -c data_15b_comp.bin -o fig_15b -col -v -scales 5 15 20 25
-
+pdpcComputeMultiScaleFeatures -i ../data/lans.ply -o data_15b -v  
+pdpcSegmentation -i ../data/lans.ply -s data_15b_scales.txt -f data_15b_features.bin -o data_15b -v 
+pdpcPostProcess -i ../data/lans.ply -s data_15b_seg.bin -r data_15b_reg.bin -c data_15b_comp.bin -o fig_15b -col -v -scales 1 2 3 4 5 6 7 8 9 10 12 15 18 20 22 25 30
